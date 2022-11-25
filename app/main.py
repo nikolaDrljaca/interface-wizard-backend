90a2028bc1a4f4632f6bd1142cf1915524be3829
nikolaDrljaca <devnikoladr@gmail.com>
2022-11-25 13:33:30 +0300

    Prep for branching.

--- app/main.py
+++ app/main.py
@@ -1,15 +1,8 @@
 from fastapi import FastAPI
-from dependencies import get_sample_model
-from dependencies import get_sample_actual
-from dependencies import get_sample_labels
-from PIL import Image
-import numpy as np
-import os
-from .routers import eval_model
+
 
 app = FastAPI()
 
-app.include_router(eval_model.router)
 
 @app.get("/")
 async def root():
