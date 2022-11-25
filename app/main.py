4c9b695ee87b405c70fc480d6fe5758852a0ee42
nikolaDrljaca <devnikoladr@gmail.com>
2022-11-25 13:32:39 +0300

    Reverted to starting point, prep for branching.

--- app/main.py
+++ app/main.py
@@ -13,21 +13,4 @@ app.include_router(eval_model.router)
 
 @app.get("/")
 async def root():
-    # main code here
-    model = get_sample_model()
-    
-    image_path = os.path.join("./data/test_images/item_0.jpg")
-    test_image = Image.open(image_path)
-    test_img_array = np.asarray(test_image)
-
-    batch = (np.expand_dims(test_img_array, 0))
-    prediction = np.argmax(model.predict(batch)[0])
-
-    label_map = get_sample_labels()
-    actuals = get_sample_actual()
-    print(label_map)
-    print(actuals)
-
-    pred = label_map[f'{prediction}']
-
-    return {"message": f"Predicted: {pred} || Actual: {actuals[0]}"}
+    return {"message": 'Welcome to ML backend.'}
