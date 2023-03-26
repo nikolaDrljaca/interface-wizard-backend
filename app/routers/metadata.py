acff9b0643574394d8afbc8a1e5cb7fbf1131fc8
nikolaDrljaca <devnikoladr@gmail.com>
2023-03-26 12:14:05 +0300

    Reformating.

--- app/routers/metadata.py
+++ app/routers/metadata.py
@@ -11,7 +11,7 @@ router = APIRouter(prefix='/api/v1/metadata')
 
 @router.get(path='/', response_model=list[str], description='Returns a list of all MODEL IDs present in the system.')
 async def get_all(db=Depends(get_db)):
-    all_metadata = await db.ml_metadata.find({},{'_id': 1}).to_list(length=None)
+    all_metadata = await db.ml_metadata.find({}, {'_id': 1}).to_list(length=None)
     all_metadata = map(lambda x: str(x['_id']), all_metadata)
     return JSONResponse(content=list(all_metadata))
 
@@ -22,11 +22,14 @@ async def get_by_id(model_id: str, db=Depends(get_db)):
     try:
         id = ObjectId(model_id)
     except:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Malformed model id.')
-    time_format = '%d/%m/%Y-%H:%M:%S' 
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail='Malformed model id.')
+    time_format = '%d/%m/%Y-%H:%M:%S'
     model_metadata = await db.ml_metadata.find_one({'_id': id}, {'_id': 0})
-    model_metadata['created'] = datetime.fromtimestamp(model_metadata['created']).strftime(time_format)
+    model_metadata['created'] = datetime.fromtimestamp(
+        model_metadata['created']).strftime(time_format)
 
-    model_metadata['expires'] = datetime.fromtimestamp(model_metadata['expires']).strftime(time_format)
+    model_metadata['expires'] = datetime.fromtimestamp(
+        model_metadata['expires']).strftime(time_format)
 
     return JSONResponse(content=model_metadata)
