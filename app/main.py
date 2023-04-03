ccb236d7388a734d413495a6190ff974fd063531
nikolaDrljaca <devnikoladr@gmail.com>
2023-04-03 13:16:08 +0300

    Added CORS middleware.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 from .routers import predictions, examples, metadata
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(predictions.router)
 app.include_router(examples.router)
 app.include_router(metadata.router)
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=['*'],
+    allow_headers=["*"]
+)
 
 
 @app.get("/")
